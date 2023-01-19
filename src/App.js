4ad4fbed320897e2f820cbc7523d1fc0c99de780
unknown <tranduyphat080300@gmail.com>
2023-01-20 00:33:32 +0200

    basic UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <video className='video' autoPlay />
+      <div className='control'>
+        <button className='btn'>Train 1</button>
+        <button className='btn'>Train 2</button>
+        <button className='btn'>Run</button>
+      </div>
     </div>
   );
 }
